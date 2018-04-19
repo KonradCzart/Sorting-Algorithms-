24721781397ebf16cef024b3b8ef1458ce1263ca
konrad <konradczart@interia.pl>
2018-04-19 12:03:08 +0200

    change insertion sort

--- Sorting-Algorithms/src/Algorithms/InsertionSort.java
+++ Sorting-Algorithms/src/Algorithms/InsertionSort.java
@@ -1,7 +1,7 @@
 package Algorithms;
 /**
  * 
- * @author Konrad
+ * @author Konrad Czart
  *
  * 
  */
@@ -22,7 +22,6 @@ public class InsertionSort <T extends Comparable<? super T>> implements SortClas
 		long start;
 		long stop;
 		start = System.nanoTime();
-		boolean shift = false;
 		
 		numberShift = 0;
     	numberComparison = 0;
@@ -37,24 +36,23 @@ public class InsertionSort <T extends Comparable<? super T>> implements SortClas
         T temp;
         int x;
         for (int i = 1; i < length; i++) {
-        	temp = tab[i];
         	x = i;
-        	while(x > 0 && temp.compareTo(tab[x-1]) < 0)
-        	{
-        		numberComparison++; //making a comparison
-        		System.err.println("Comparison: current="+ temp + " vs tab["+ (x-1) +"]=" + tab[x-1]); // err print comparison
-        		tab[x] = tab[x-1];
-        		System.err.println("Shift: tab["+x+"] = " + tab[x-1]); //err print shift
+        	
+        	numberComparison++; //making a comparison
+        	if(tab[i].compareTo(tab[i-1]) < 0) {
+            	temp = tab[i];
+            	tab[x] = tab[x-1];
         		numberShift++; //making a shift
         		x--;
-        		shift = true;
-        	}
-        	numberComparison++; //making a comparison
-        	if(x>0)
-        		System.err.println("Comparison: current="+ temp + " vs tab["+ (x-1) +"]=" + tab[x-1]); // err print comparison
-        	if(shift) {
+            	while(x > 0 && temp.compareTo(tab[x-1]) < 0)
+            	{
+            		numberComparison++; //making a comparison
+            		tab[x] = tab[x-1];
+            		numberShift++; //making a shift
+            		x--;
+            	}
+            	
             	tab[x] = temp;
-            	System.err.println("Shift: tab["+x+"] = " + temp); //err print shift
             	numberShift++; //making a shift
         	}
         }
@@ -69,7 +67,6 @@ public class InsertionSort <T extends Comparable<? super T>> implements SortClas
 		long start;
 		long stop;
 		start = System.nanoTime();
-		boolean shift = false;
 		
 		numberShift = 0;
     	numberComparison = 0;
@@ -84,25 +81,23 @@ public class InsertionSort <T extends Comparable<? super T>> implements SortClas
         T temp;
         int x;
         for (int i = 1; i < length; i++) {
-        	temp = tab[i];
         	x = i;
         	
-        	while(x > 0 && temp.compareTo(tab[x-1]) > 0)
-        	{
-        		numberComparison++; //making a comparison
-        		System.err.println("Comparison: current="+ temp + " vs tab["+ (x-1) +"]=" + tab[x-1]); // err print comparison
-        		tab[x] = tab[x-1];
-        		System.err.println("Shift: tab["+x+"] = " + tab[x-1]); //err print shift
+        	numberComparison++; //making a comparison
+        	if(tab[i].compareTo(tab[i-1]) < 0) {
+            	temp = tab[i];
+            	tab[x] = tab[x-1];
         		numberShift++; //making a shift
         		x--;
-        		shift = true;
-        	}
-        	numberComparison++; //making a comparison
-        	if(x>0)
-        		System.err.println("Comparison: current="+ temp + " vs tab["+ (x-1) +"]=" + tab[x-1]); // err print comparison
-        	if(shift) {
+            	while(x > 0 && temp.compareTo(tab[x-1]) < 0)
+            	{
+            		numberComparison++; //making a comparison
+            		tab[x] = tab[x-1];
+            		numberShift++; //making a shift
+            		x--;
+            	}
+            	
             	tab[x] = temp;
-            	System.err.println("Shift: tab["+x+"] = " + temp); //err print shift
             	numberShift++; //making a shift
         	}
         }
